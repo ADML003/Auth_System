164846e002dd4d459299513b06b5f5359c0ac1c0
Aditya Malhotra <adml003.pro@gmail.com>
2025-03-15 16:58:25 +0530

    final log in and sign in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,9 @@
-"use client"; // making it a client component
-
+"use client";
 import Link from "next/link";
 import React, { useEffect } from "react";
-import { userRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import axios from "axios";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -12,7 +11,6 @@ export default function LoginPage() {
     email: "",
     password: "",
   });
-
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
@@ -20,12 +18,11 @@ export default function LoginPage() {
     try {
       setLoading(true);
       const response = await axios.post("/api/users/login", user);
-      console.log("login success", response.data);
-
-      toast.success("login successful");
+      console.log("Login success", response.data);
+      toast.success("Login success");
       router.push("/profile");
     } catch (error: any) {
-      console.log("login faield", error.message);
+      console.log("Login failed", error.message);
       toast.error(error.message);
     } finally {
       setLoading(false);
@@ -39,47 +36,37 @@ export default function LoginPage() {
       setButtonDisabled(true);
     }
   }, [user]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1> Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
-      {/* <label htmlFor="username"> username</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-        id="username"
-        type="text"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="username"
-      /> */}
 
-      <label htmlFor="email"> email</label>
+      <label htmlFor="email">email</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
         value={user.email}
         onChange={(e) => setUser({ ...user, email: e.target.value })}
         placeholder="email"
       />
-
-      <label htmlFor="password"> password</label>
+      <label htmlFor="password">password</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="password"
         value={user.password}
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
-
       <button
-        className="p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
         onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Log in
+        Login here
       </button>
-      <Link href="/signup">Visit sign up page</Link>
+      <Link href="/signup">Visit Signup page</Link>
     </div>
   );
 }
